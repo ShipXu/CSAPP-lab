8ab306e9d4f454eaa4e025163cfd60a519aff437
ShipXu <shipxp@csu.edu.cn>
2020-03-11 18:24:26 +0800

    add final trans

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -296,40 +296,285 @@ void column_trans(int M, int N, int A[N][M], int B[M][N])
 }
 
 /* 
- * half-trans - A half transpose function.
- */
-char half_trans_desc[] = "half scan transpose";
-void half_trans(int M, int N, int A[N][M], int B[M][N])
+ * final-trans - A final transpose function.
+*/
+char final_trans_desc[] = "final scan transpose";
+void final_trans(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
-    // int ii, work_amount_n;
-    // int bsize;
-    
-    // bsize = 8;
-    // work_amount_n = (N / bsize) * bsize;
+    int i, j;
+    int ii, jj;
+    int a0, a1, a2, a3, a4, a5, a6, a7;
 
-    for (i = 0; i < N; i++) {
-        for (j = i; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
+    for (ii = 0; ii < N; ii += 8) {
+        for (jj = 0; jj < M; jj += 8) {
+            if (ii == jj) {
+                for (i = ii; i < ii + 4; i++) {
+                    a0 = A[i][jj];
+                    a1 = A[i][jj + 1];
+                    a2 = A[i][jj + 2];
+                    a3 = A[i][jj + 3];
+                    a4 = A[i][jj + 4];
+                    a5 = A[i][jj + 5];
+                    a6 = A[i][jj + 6];
+                    a7 = A[i][jj + 7];
+                    B[jj][i] = a0;
+                    B[jj + 1][i] = a1;
+                    B[jj + 2][i] = a2;
+                    B[jj + 3][i] = a3;
+                    B[jj][i + 4] = a4;
+                    B[jj + 1][i + 4] = a5;
+                    B[jj + 2][i + 4] = a6;
+                    B[jj + 3][i + 4] = a7;
+                }
+
+                for (i = ii + 4; i < ii + 8; i++) {
+                    a0 = A[i][jj];
+                    a1 = A[i][jj + 1];
+                    a2 = A[i][jj + 2];
+                    a3 = A[i][jj + 3];
+                    a4 = A[i][jj + 4];
+                    a5 = A[i][jj + 5];
+                    a6 = A[i][jj + 6];
+                    a7 = A[i][jj + 7];
+                    B[jj + 4][i - 4] = a0;
+                    B[jj + 4 + 1][i - 4] = a1;
+                    B[jj + 4 + 2][i - 4] = a2;
+                    B[jj + 4 + 3][i - 4] = a3;
+                    B[jj + 4][i] = a4;
+                    B[jj + 4 + 1][i] = a5;
+                    B[jj + 4 + 2][i] = a6;
+                    B[jj + 4 + 3][i] = a7;
+                }
+
+                for (j = jj; j < jj + 4; j++) {
+                    a0 = B[j + 4][ii];
+                    a1 = B[j + 4][ii + 1];
+                    a2 = B[j + 4][ii + 2];
+                    a3 = B[j + 4][ii + 3];
+                    a4 = B[j][ii + 4];
+                    a5 = B[j][ii + 5];
+                    a6 = B[j][ii + 6];
+                    a7 = B[j][ii + 7];
+                    B[j][ii + 4] = a0;
+                    B[j][ii + 5] = a1;
+                    B[j][ii + 6] = a2;
+                    B[j][ii + 7] = a3;
+                    B[j + 4][ii] = a4;
+                    B[j + 4][ii + 1] = a5;
+                    B[j + 4][ii + 2] = a6;
+                    B[j + 4][ii + 3] = a7;
+                }
+            }
+            else {
+                for (i = ii; i < ii + 4; i++) {
+                    a0 = A[i][jj];
+                    a1 = A[i][jj + 1];
+                    a2 = A[i][jj + 2];
+                    a3 = A[i][jj + 3];
+
+                    a4 = A[i][jj + 4];
+                    a5 = A[i][jj + 5];
+                    a6 = A[i][jj + 6];
+                    a7 = A[i][jj + 7];
+
+                    B[jj][i] = a0;
+                    B[jj + 1][i] = a1;
+                    B[jj + 2][i] = a2;
+                    B[jj + 3][i] = a3;
+
+                    B[jj][i + 4] = a4;
+                    B[jj + 1][i + 4] = a5;
+                    B[jj + 2][i + 4] = a6;
+                    B[jj + 3][i + 4] = a7;
+                }
+
+                for (j = 0; j < 4; j++) {
+                    a0 = B[jj][ii + 4 + j];
+                    a1 = B[jj + 1][ii + 4 + j];
+                    a2 = B[jj + 2][ii + 4 + j];
+                    a3 = B[jj + 3][ii + 4 + j];
+
+                    a4 = A[ii + 4 + j][jj];
+                    a5 = A[ii + 4 + j][jj + 1];
+                    a6 = A[ii + 4 + j][jj + 2];
+                    a7 = A[ii + 4 + j][jj + 3];
+
+                    B[jj][ii + 4 + j] = a4;
+                    B[jj + 1][ii + 4 + j] = a5;
+                    B[jj + 2][ii + 4 + j] = a6;
+                    B[jj + 3][ii + 4 + j] = a7;
+
+                    B[jj + 4 + j][ii] = a0;
+                    B[jj + 4 + j][ii + 1] = a1;
+                    B[jj + 4 + j][ii + 2] = a2;
+                    B[jj + 4 + j][ii + 3] = a3;
+
+                    a4 = A[ii + 4 + j][jj + 4];
+                    a5 = A[ii + 4 + j][jj + 4 + 1];
+                    a6 = A[ii + 4 + j][jj + 4 + 2];
+                    a7 = A[ii + 4 + j][jj + 4 + 3];
+
+                    B[jj + 4 + j][ii + 4] = a4;
+                    B[jj + 4 + j][ii + 4 + 1] = a5;
+                    B[jj + 4 + j][ii + 4 + 2] = a6;
+                    B[jj + 4 + j][ii + 4 + 3] = a7;
+                }
+
+                for (i = 0; i < 4; i++) {
+                    for (j = 0; j < i; j ++) {
+                        a0 = B[jj + 4 + i][ii + j];
+                        a1 = B[jj + 4 + j][ii + i];
+                        B[jj + 4 + i][ii + j] = a1;
+                        B[jj + 4 + j][ii + i] = a0;
+                    }
+                }
+
+                for (i = 0; i < 4; i++) {
+                    for (j = 0; j < i; j ++) {
+                        a0 = B[jj + 4 + j][ii + 4 + i];
+                        a1 = B[jj + 4 + i][ii + 4 + j];
+                        B[jj + 4 + i][ii + 4 + j] = a0;
+                        B[jj + 4 + j][ii + 4 + i] = a1;
+                    }
+                }
+            }
         }
-
     }
+}
 
-    for (j = 0; j < M; j++) {
-        for (i = j + 1; i < N; i++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
+/* 
+ * copy-trans - A copy transpose function.
+ */
+char copy_trans_desc[] = "copy scan transpose";
+void copy_trans(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+    int ii, jj;
+    int a0, a1, a2, a3, a4, a5, a6, a7;
+    // int 
+
+    for (ii = 0; ii < N; ii += 8) {
+        for (jj = 0; jj < M; jj += 8) {
+            for (i = ii; i < ii + 4; i++) {
+                a0 = A[i][jj];
+                a1 = A[i][jj + 1];
+                a2 = A[i][jj + 2];
+                a3 = A[i][jj + 3];
+
+                a4 = A[i][jj + 4];
+                a5 = A[i][jj + 5];
+                a6 = A[i][jj + 6];
+                a7 = A[i][jj + 7];
+
+                B[jj][i] = a0;
+                B[jj + 1][i] = a1;
+                B[jj + 2][i] = a2;
+                B[jj + 3][i] = a3;
+
+                B[jj][i + 4] = a4;
+                B[jj + 1][i + 4] = a5;
+                B[jj + 2][i + 4] = a6;
+                B[jj + 3][i + 4] = a7;
+            }
+
+            for (j = 0; j < 4; j++) {
+                a0 = B[jj][ii + 4 + j];
+                a1 = B[jj + 1][ii + 4 + j];
+                a2 = B[jj + 2][ii + 4 + j];
+                a3 = B[jj + 3][ii + 4 + j];
+
+                a4 = A[ii + 4 + j][jj];
+                a5 = A[ii + 4 + j][jj + 1];
+                a6 = A[ii + 4 + j][jj + 2];
+                a7 = A[ii + 4 + j][jj + 3];
+
+                B[jj][ii + 4 + j] = a4;
+                B[jj + 1][ii + 4 + j] = a5;
+                B[jj + 2][ii + 4 + j] = a6;
+                B[jj + 3][ii + 4 + j] = a7;
+
+                B[jj + 4 + j][ii] = a0;
+                B[jj + 4 + j][ii + 1] = a1;
+                B[jj + 4 + j][ii + 2] = a2;
+                B[jj + 4 + j][ii + 3] = a3;
+
+                a4 = A[ii + 4 + j][jj + 4];
+                a5 = A[ii + 4 + j][jj + 4 + 1];
+                a6 = A[ii + 4 + j][jj + 4 + 2];
+                a7 = A[ii + 4 + j][jj + 4 + 3];
+
+                B[jj + 4 + j][ii + 4] = a4;
+                B[jj + 4 + j][ii + 4 + 1] = a5;
+                B[jj + 4 + j][ii + 4 + 2] = a6;
+                B[jj + 4 + j][ii + 4 + 3] = a7;
+            }
+
+            // a0 = ;
+            // a1 = ;
+            // a2 = ;
+            // a3 = ;
+            for (i = 0; i < 4; i++) {
+                for (j = 0; j < i; j ++) {
+                    a0 = B[jj + 4 + i][ii + j];
+                    a1 = B[jj + 4 + j][ii + i];
+                    B[jj + 4 + i][ii + j] = a1;
+                    B[jj + 4 + j][ii + i] = a0;
+                }
+            }
+
+            for (i = 0; i < 4; i++) {
+                for (j = 0; j < i; j ++) {
+                    a0 = B[jj + 4 + j][ii + 4 + i];
+                    a1 = B[jj + 4 + i][ii + 4 + j];
+                    B[jj + 4 + i][ii + 4 + j] = a0;
+                    B[jj + 4 + j][ii + 4 + i] = a1;
+                }
+            }
+            // for (j = 0; j < 4; j++) {
+            //     a0 = B[jj + 4 + j][ii];
+            //     a1 = B[jj + 4 + j][ii + 1];
+            //     a2 = B[jj + 4 + j][ii + 2];
+            //     a3 = B[jj + 4 + j][ii + 3];
+
+            //     a4 = B[jj + 4][ii + j];
+            //     a5 = B[jj + 4 + 1][ii + j];
+            //     a6 = B[jj + 4 + 2][ii + j];
+            //     a7 = B[jj + 4 + 3][ii + j];
+
+            //     B[jj + 4 + j][ii] = a4;
+            //     B[jj + 4 + j][ii + 1] = a5;
+            //     B[jj + 4 + j][ii + 2] = a6;
+            //     B[jj + 4 + j][ii + 3] = a7;
+
+            //     B[jj + 4][ii + j] = a0;
+            //     B[jj + 4 + 1][ii + j] = a1;
+            //     B[jj + 4 + 2][ii + j] = a2;
+            //     B[jj + 4 + 3][ii + j] = a3;
+            // }
+
+            // for (j = 0; j < 4; j++) {
+            //     a0 = B[jj + 4 + j][ii + 4];
+            //     a1 = B[jj + 4 + j][ii + 4 + 1];
+            //     a2 = B[jj + 4 + j][ii + 4 + 2];
+            //     a3 = B[jj + 4 + j][ii + 4 + 3];
+
+            //     a4 = B[jj + 4][ii + 4 + j];
+            //     a5 = B[jj + 4 + 1][ii + 4 + j];
+            //     a6 = B[jj + 4 + 2][ii + 4 + j];
+            //     a7 = B[jj + 4 + 3][ii + 4 + j];
+
+            //     B[jj + 4 + j][ii + 4] = a4;
+            //     B[jj + 4 + j][ii + 4 + 1] = a5;
+            //     B[jj + 4 + j][ii + 4 + 2] = a6;
+            //     B[jj + 4 + j][ii + 4 + 3] = a7;
+
+            //     B[jj + 4][ii + 4 + j] = a0;
+            //     B[jj + 4 + 1][ii + 4 + j] = a1;
+            //     B[jj + 4 + 2][ii + 4 + j] = a2;
+            //     B[jj + 4 + 3][ii + 4 + j] = a3;
+            // }
         }
     }
-    // for (ii = 0; ii < work_amount_n; ii += bsize) {
-    //     for (j = 0; j < M; j++) {
-    //         for (i = ii; i < ((ii + bsize <= work_amount_n)?(ii + bsize):(work_amount_n)); i++) {
-    //             tmp = A[i][j];
-    //             B[j][i] = tmp;
-    //         }
-    //     }
-    // }
 }
 
 /*
@@ -348,6 +593,8 @@ void registerFunctions()
     registerTransFunction(trans, trans_desc); 
 
     registerTransFunction(block_trans, block_trans_desc);
+    registerTransFunction(copy_trans, copy_trans_desc);
+    registerTransFunction(final_trans, final_trans_desc);
     // registerTransFunction(half_trans, half_trans_desc);
     // registerTransFunction(column_trans, column_trans_desc); 
 
